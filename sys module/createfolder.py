ed9be4ecf1cc1b3197fc3b2d943ec7094c5a87f9
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-02-13 16:16:03 +0400

    codes updated

--- sys module/createfolder.py
+++ sys module/createfolder.py
@@ -13,3 +13,28 @@ elif sys.argv[1] == "delete" and sys.argv[2] == "folder":
     parent_directory = "C:/Users/user/Desktop/python/file"
     path = os.path.join(parent_directory, directory)
     os.rmdir(path) # delete folder
+
+Task2
+import sys
+inputs=[]
+if len(sys.argv)>1:
+    a = sys.argv[1]
+    if a == "add":
+        title1 = input("Enter Series Name: ")
+        title2 = input("Enter director name: ")
+        title3 = input("Enter series of episodes: ")
+        title4 = input("Enter genre: ")
+        b = [title1,title2,title3,title4]
+        inputs.append(b)
+        print(*inputs)
+
+
+    else:
+        print("please write add")
+
+try:
+    with open("C:/Users/user/Desktop/python/newtext.txt", "a") as file:
+        for item in inputs:
+            file.write(str(item) + "\n")
+except Exception:
+    print("An error occurred while writing to the file:",)
